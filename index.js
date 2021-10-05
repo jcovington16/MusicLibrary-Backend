7df14a9ed126b8f25107bc4fe7da6a116864cc72
techniqueo <67283557+techniqueo@users.noreply.github.com>
2021-10-04 20:56:15 -0500

    Added post, put, and delete files.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const repoContext = require('./repository/repository-wrapper');
-const cors = require('cors');
-const { validateSongs } = require('./middleware/music-validation')
+const express = require("express");
+const repoContext = require("./repository/repository-wrapper");
+const cors = require("cors");
+const { validateSongs } = require("./middleware/music-validation");
 
 const app = express();
 
@@ -9,10 +9,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
-    console.log("Server started. Listening on port 3000");
+  console.log("Server started. Listening on port 3000");
 });
 
-app.get('/api/music', (req, res) => {
-    const music = repoContext.songs.findAllSongs()
-    return res.send(music);
+app.get("/api/music", (req, res) => {
+  const music = repoContext.songs.findAllSongs();
+  return res.send(music);
+});
+
+app.post("/api/music", (req, res) => {
+  const newSong = req.body;
+  const addedSongs = repoContext.songs.createSong(newSong);
+  return res.send(addedSongs);
+});
+
+app.put("/api/music", (req, res) => {
+  const songTracksToUpdate = req.body;
+  const updatedSonglist = repoContext.songs.updateSong(songTracksToUpdate);
+  return res.send(updatedSonglist);
+});
+
+app.delete("/api/music", (req, res) => {
+  const updatedDataSet = repoContext.products.deleteSong(id);
+  return res.send(updatedDataSet);
 });
