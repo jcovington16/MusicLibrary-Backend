6d3b960a409e8db7f2c6aa6774c3c7861b7d7581
jcovington16 <covingtonjoshua7@gmail.com>
2021-10-04 20:13:33 -0600

    we can now grab the song by its id for our get, put and delete requests. Will now start working on tieing it to the front end portion.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const repoContext = require("./repository/repository-wrapper");
-const cors = require("cors");
-const { validateSongs } = require("./middleware/music-validation");
+const express = require('express');
+const repoContext = require('./repository/repository-wrapper');
+const cors = require('cors');
+const { validateSongs } = require('./middleware/music-validation');
 
 const app = express();
 
@@ -9,13 +9,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
-  console.log("Server started. Listening on port 3000");
+    console.log("Server started. Listening on port 3000");
 });
 
-app.get("/api/music", (req, res) => {
-  const music = repoContext.songs.findAllSongs();
-  return res.send(music);
+app.get('/api/music', (req, res) => {
+    const music = repoContext.songs.findAllSongs()
+    return res.send(music);
 });
+
+app.get('/api/music/:id', (req, res) => {
+    const id = req.params.id;
+    const musicId = repoContext.songs.findSongById(id);
+    return res.send(musicId);
+})
 
 app.post("/api/music", (req, res) => {
   const newSong = req.body;
@@ -23,13 +29,15 @@ app.post("/api/music", (req, res) => {
   return res.send(addedSongs);
 });
 
-app.put("/api/music", (req, res) => {
+app.put("/api/music/:id", (req, res) => {
+  const id = req.params.id;
   const songTracksToUpdate = req.body;
   const updatedSonglist = repoContext.songs.updateSong(songTracksToUpdate);
   return res.send(updatedSonglist);
 });
 
-app.delete("/api/music", (req, res) => {
-  const updatedDataSet = repoContext.products.deleteSong(id);
-  return res.send(updatedDataSet);
+app.delete("/api/music/:id", (req, res) => {
+    const id = req.params.id;
+    const updatedDataSet = repoContext.songs.deleteSong(id);
+    return res.send(updatedDataSet);
 });
